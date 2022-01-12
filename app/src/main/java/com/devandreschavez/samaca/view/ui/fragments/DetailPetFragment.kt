da8c7ecb2a9ee182a21ce1aa3f45c6b6e429a3e9
Andres Chavez <andres.cz.chavez@gmail.com>
2022-01-11 21:18:17 -0500

    Mejoras esteticas en el detalle y función de llamar...

--- app/src/main/java/com/devandreschavez/samaca/view/ui/fragments/DetailPetFragment.kt
+++ app/src/main/java/com/devandreschavez/samaca/view/ui/fragments/DetailPetFragment.kt
@@ -1,10 +1,18 @@
 package com.devandreschavez.samaca.view.ui.fragments
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.devandreschavez.samaca.R
@@ -38,5 +46,12 @@ class DetailPetFragment : BottomSheetDialogFragment() {
             binding.tvPhoneUserDetail.text = user.phone
 
         }
+        binding.tvPhoneUserDetail.setOnClickListener {
+           goPhone()
+        }
+    }
+    private fun goPhone(){
+        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${binding.tvPhoneUserDetail.text}"))
+        startActivity(intent)
     }
 }
